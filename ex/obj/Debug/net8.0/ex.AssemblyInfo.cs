0a46300fcb6e7686a5690600018057c64da31e14
Atajan <atajanjumayev1991@gmail.com>
2024-04-07 09:44:57 +0800

    create matrix exercise

--- ex/obj/Debug/net8.0/ex.AssemblyInfo.cs
+++ ex/obj/Debug/net8.0/ex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de14c47b54fd252cd884a608738177058c4d5f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
